d9b989a0846405407bc5b6890073d208fcec745d
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-10 15:04:42 +0300

    Construct the response out of the controllers

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { LoginDto } from './dto/login.dto';
+import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { Public } from './decorators/public.decorator';
+import { CreateUserDto } from 'src/users/dto/user.dto';
+import { Body, Controller, Post } from '@nestjs/common';
+import { BaseResponseHandler } from 'src/common/utils/baseResponseHandler';
+
+@Controller('api/auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
+
+  @Public()
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const result = await this.authService.login(loginDto);
+    return BaseResponseHandler.create(200, 'Login successful', result);
+  }
+
+  @Public()
+  @Post()
+  async register(@Body() createUserDto: CreateUserDto) {
+    const user = await this.authService.create(createUserDto);
+    return BaseResponseHandler.create(201, 'User created successfully', user);
+  }
+}
