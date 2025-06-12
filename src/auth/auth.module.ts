ff50a532e8c3b715578de2fe33dd8591e48cc248
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-12 20:03:19 +0300

    Add CartModule to AuthModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { CartModule } from 'src/cart/cart.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule } from '@nestjs/config';
       signOptions: { expiresIn: '1h' },
     }),
     UsersModule,
+    CartModule,
   ],
   providers: [
     {
