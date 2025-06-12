7b01d2b5a8355d89582d144f74b6ea05522da94b
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-12 13:10:35 +0300

    Add public Decorators

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,6 +12,7 @@ import {
 } from '@nestjs/common';
 
 import { ProductsService } from './products.service';
+import { Public } from 'src/auth/decorators/public.decorator';
 import { EUserRole } from 'src/users/enums/user.enum';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
@@ -22,6 +23,7 @@ import { CreateProductDto, UpdateProductDto } from './dto/product.dto';
 import { CurrentUser } from 'src/auth/decorators/currentUser.decorator';
 import { BaseResponse } from 'src/common/interfaces/responses.interface';
 import { BaseResponseHandler } from 'src/common/utils/baseResponseHandler';
+import { isUndefined } from 'util';
 
 @UseGuards(AuthGuard, RolesGuard)
 @Controller('products')
@@ -33,8 +35,9 @@ export class ProductsController {
   async create(
     @Body() createProductDto: CreateProductDto,
     @CurrentUser() currentUser: IUser,
-  ): Promise<BaseResponse<Product>> {
+  ): Promise<BaseResponse<ProductDocument>> {
     createProductDto.supplier = currentUser._id.toString();
+
     const createdProduct = await this.productsService.create(createProductDto);
     return BaseResponseHandler.create(
       HttpStatus.CREATED,
@@ -44,7 +47,8 @@ export class ProductsController {
   }
 
   @Get()
-  async findAll(): Promise<BaseResponse<Product[]>> {
+  @Public()
+  async findAll(): Promise<BaseResponse<ProductDocument[]>> {
     const products = await this.productsService.findAll();
     return BaseResponseHandler.create(
       HttpStatus.OK,
@@ -54,7 +58,9 @@ export class ProductsController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<BaseResponse<Product>> {
+  async findOne(
+    @Param('id') id: string,
+  ): Promise<BaseResponse<ProductDocument>> {
     const product = await this.productsService.findOne(id);
     return BaseResponseHandler.create(
       HttpStatus.OK,
@@ -69,7 +75,7 @@ export class ProductsController {
     @Param('id') id: string,
     @Body() updateProductDto: UpdateProductDto,
     @CurrentUser() currentUser: IUser,
-  ): Promise<BaseResponse<Product>> {
+  ): Promise<BaseResponse<ProductDocument>> {
     const currentProduct = await this.productsService.findOne(id);
 
     if (
@@ -90,7 +96,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
-  @Roles(EUserRole.SUPPLIER, EUserRole.ADMIN, EUserRole.USER)
+  @Roles(EUserRole.SUPPLIER, EUserRole.ADMIN)
   async remove(
     @Param('id') id: string,
     @CurrentUser() currentUser: IUser,
