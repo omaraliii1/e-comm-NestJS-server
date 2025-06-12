036896fa2da97f7ed30041597919815bf7790424
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-12 19:59:59 +0300

    Refactor ProductsModule to use forwardRef for UsersModule and clean up imports

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,30 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product, ProductSchema } from './entities/product.entity';
-import { UsersModule } from '../users/users.module'; // Import UsersModule
+import { UsersModule } from '../users/users.module';
 import {
   Category,
   CategorySchema,
 } from '../categories/entities/category.entity';
 import { CategoriesModule } from 'src/categories/categories.module';
+
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
-
-    UsersModule,
     MongooseModule.forFeature([
+      { name: Product.name, schema: ProductSchema },
       { name: Category.name, schema: CategorySchema },
     ]),
+    forwardRef(() => UsersModule),
     CategoriesModule,
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
-  exports: [
-    ProductsService,
-
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
-  ],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
