12347323a751c88a4d76287202c588f8d951d675
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-12 19:50:49 +0300

    Import CartModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-// import { CategoriesModule } from './categories/categories.module';
-// import { CartModule } from './carts/carts.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CartModule } from './cart/cart.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    UsersModule,
-    ProductsModule,
-    // CategoriesModule,
-    // CartModule,
     MongooseModule.forRoot(
       `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`,
       { dbName: process.env.DB_NAME },
     ),
     AuthModule,
+    UsersModule,
     ProductsModule,
     CategoriesModule,
+    CartModule,
+    ReviewsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
