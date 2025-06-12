2f6c6df5670c3a0601f7971dc5dbe22b5d13771f
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-12 23:42:09 +0300

    Add virtual field 'products' to Category schema and enable virtuals in toObject/toJSON methods

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type CategoryDocument = HydratedDocument<Category>;
 
@@ -13,3 +13,12 @@ export class Category {
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
+
+CategorySchema.virtual('products', {
+  ref: 'Product',
+  localField: '_id',
+  foreignField: 'category',
+});
+
+CategorySchema.set('toObject', { virtuals: true });
+CategorySchema.set('toJSON', { virtuals: true });
