e80650b0e26e42bc3a2dcdcceb3e77cd6d80b896
omaraliii1 <omaraliyassin18@gmail.com>
2025-06-12 23:41:41 +0300

    Remove optional product field and totalPrice from BaseCartDto

--- src/cart/dto/cart.dto.ts
+++ src/cart/dto/cart.dto.ts
@@ -11,16 +11,6 @@ export class BaseCartDto {
   @IsMongoId()
   @IsNotEmpty()
   user: string;
-
-  @IsMongoId()
-  @IsOptional()
-  product?: string;
-
-  @IsNumber()
-  @Min(0)
-  totalPrice: number;
 }
 
 export class CreateCartDto extends BaseCartDto {}
-
-export class UpdateCartDto extends PartialType(BaseCartDto) {}
